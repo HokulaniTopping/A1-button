f06c9c15621b3b99ed404657ef72b18c1e163015
HokulaniTopping <hokulanigit@gmail.com>
2025-04-15 13:01:35 -0700

    fixed spacing on each e;ement

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,9 @@ import { radioButton } from "./widgets/radiobutton";
 
 let w = new Window(window.innerHeight-10,'100%');
 
+
+
+
 let lbl1= new Heading(w);
 lbl1.tabindex = 1;
 lbl1.fontSize = 16;
@@ -42,7 +45,7 @@ btn.onClick(() => {
 
 const checkBox = new CheckBox(w);
 checkBox.text = "Accept Terms";
-checkBox.move(10, 70);
+checkBox.move(10, 80);
 
 
 checkBox.onClick(() => {
@@ -66,7 +69,7 @@ const group = {}; // This could be an object or array depending on your needs
 // Create multiple radio buttons with the same group
 const radiobutton1 = new radioButton(w, group);
 radiobutton1.text = "Option 1";
-radiobutton1.move(10, 100);
+radiobutton1.move(10, 150);
 
 radiobutton1.onClick(() => {
     console.log(radiobutton1.checked ? "Option 1 is selected" : "Option 1 is unselected");
@@ -77,7 +80,7 @@ radiobutton1.fontSize = 14;
 
 const radiobutton2 = new radioButton(w, group);
 radiobutton2.text = "Option 2";
-radiobutton2.move(10, 140);
+radiobutton2.move(10, 180);
 
 radiobutton2.onClick(() => {
     console.log(radiobutton2.checked ? "Option 2 is selected" : "Option 2 is unselected");
@@ -88,7 +91,7 @@ radiobutton2.fontSize = 14;
 
 const radiobutton3 = new radioButton(w, group);
 radiobutton3.text = "Option 3";
-radiobutton3.move(10, 180);
+radiobutton3.move(10, 210);
 
 radiobutton3.onClick(() => {
     console.log(radiobutton3.checked ? "Option 3 is selected" : "Option 3 is unselected");
